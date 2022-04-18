0b4628fb7a7bfc8f98b65bcef26b4bf733fff06d
NinoLombard <maxime.lombard@live.com.mx>
2022-04-18 20:34:23 +0200

    Mise a jour du code d'importation des tables

--- Script/importation_donnees.py
+++ Script/importation_donnees.py
@@ -2,7 +2,6 @@ import sqlite3
 import csv
 import demoji
 
-
 # Fonction utile pour matching_answers_qcm
 def is_integer(n):
    try:
@@ -33,7 +32,7 @@ titre = "Entendre la France"
 conn.execute('INSERT INTO sondage VALUES("{}", "{}")'.format(id, titre))
 
 # Table matching_questions
-chemin_fichier = "./Data_entendre_la_france/matching_questions.csv"
+chemin_fichier = "../Data_entendre_la_france/matching_questions.csv"
 
 with open(chemin_fichier, "r", encoding="utf-8") as csvfile:
    fichier_lu = csv.reader(csvfile, delimiter=";")
@@ -62,7 +61,7 @@ for li in liste:
    conn.execute('INSERT INTO questions_posees VALUES("{}", "{}", "{}")'.format(1, li[0], compteur))
 
 # Table users
-chemin_fichier = "./Data_entendre_la_france/users.csv"
+chemin_fichier = "../Data_entendre_la_france/users.csv"
 
 with open(chemin_fichier, 'r', encoding="utf-8") as csvfile:
    fichier_lu = csv.reader(csvfile, delimiter=";")
@@ -87,7 +86,7 @@ with open(chemin_fichier, 'r', encoding="utf-8") as csvfile:
        conn.execute('INSERT INTO users VALUES("{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}")'.format(id, code_postal, commune, type_commune, nom_departement, departement, genre, tranche_age, formation, profession, taille_org, position_gj))
 
 # Table matching_answers_qcm
-chemin_fichier = "./Data_entendre_la_france/matching_answers_qcm.csv"
+chemin_fichier = "../Data_entendre_la_france/matching_answers_qcm.csv"
 
 with open(chemin_fichier, 'r', encoding="utf-8") as csvfile:
    fichier_lu = csv.reader(csvfile, delimiter=";")
@@ -98,6 +97,8 @@ with open(chemin_fichier, 'r', encoding="utf-8") as csvfile:
    liste_lignes.pop(0)
 
    id_max = 0
+
+   correspondance_question_max = {}  # Utilisé pour answers_qcm
    for ind_ligne in range(0, len(liste_lignes)):
        MQid = correspondance_question_id[liste_lignes[ind_ligne][0]]
        title = liste_lignes[ind_ligne][2]
@@ -110,10 +111,51 @@ with open(chemin_fichier, 'r', encoding="utf-8") as csvfile:
                id_max += 1
                id = int(code_emo.split(":")[-1])
            else:
+               correspondance_question_max[MQid] = id_max  # Utilisé pour answers_qcm
                id = id_max+1
                id_max = 0
        conn.execute('INSERT INTO matching_answers_qcm VALUES("{}", "{}", "{}")'.format(id, MQid, title))
 
 
-# Construction UI et stockage des reponses par question
+# Table answers_free
+chemin_fichier = "../Data_entendre_la_france/answers_free.csv"
+
+with open(chemin_fichier, 'r', encoding="utf-8") as csvfile:
+   fichier_lu = csv.reader(csvfile, delimiter=";")
+   liste_lignes = []
+   for row in fichier_lu:
+       liste_lignes.append(row)
+   # supprime la première ligne correspondant au titre
+   liste_lignes.pop(0)
+
+   for ind_ligne in range(0, len(liste_lignes)):
+       Uid = liste_lignes[ind_ligne][0]
+       MQid = correspondance_question_id[liste_lignes[ind_ligne][1]]
+       answer = liste_lignes[ind_ligne][2]
+       conn.execute('INSERT INTO answers_free VALUES("{}", "{}", "{}", "{}")'.format(1, Uid, MQid, answer.replace("\"", "'")))
+
+# Table answers_qcm
+chemin_fichier = "../Data_entendre_la_france/answers_qcm.csv"
+
+with open(chemin_fichier, 'r', encoding="utf-8") as csvfile:
+   fichier_lu = csv.reader(csvfile, delimiter=";")
+   liste_lignes = []
+   for row in fichier_lu:
+       liste_lignes.append(row)
+   # supprime la première ligne correspondant au titre
+   liste_lignes.pop(0)
+
+   for ind_ligne in range(0, len(liste_lignes)):
+       Uid = liste_lignes[ind_ligne][0]
+       MQid = correspondance_question_id[liste_lignes[ind_ligne][1]]
+       MAid = 0
+
+       emoji_code = demoji.findall(liste_lignes[ind_ligne][2])
+
+       for code_emo in emoji_code.values():
+           if is_integer(code_emo.split(":")[-1]):
+               MAid = int(code_emo.split(":")[-1])
+           else:
+               MAid = correspondance_question_max[MQid]+1
+       conn.execute('INSERT INTO answers_qcm VALUES("{}", "{}", "{}", "{}")'.format(1, Uid, MQid, MAid))
 
